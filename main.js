d4f7d55a36bd266bb9adc92349fec9bd8c140d0a
Jollen Chen <jollen@moko365.com>
2015-07-19 11:50:41 +0800

    use backbone

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
-$(document).ready(function() {
-	$('.col').click(function(event) {
-		$(this).html('<h2>O</h2>');
-	});
-});
+/**
+ * SETUP
+ **/
+  var app = app || {};
+
+/**
+ * MODELS
+ **/
+
+
+/**
+ * VIEWS
+ **/
+  app.MessageView = Backbone.View.extend({
+  	el: '#main',
+    events: {
+    	'click .col': 'click' 
+    },
+    click: function(e) {
+    	var t = this.$el.find(e.target);
+    	t.html('Yes');
+    }
+  });
+
+/**
+ * BOOTUP
+ **/
+  $(document).ready(function() {
+    app.messageView = new app.MessageView();
+  });
